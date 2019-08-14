8977fa8b10807b5b7bac08b16b747e5be15e6769
alex_yang <2041462766@qq.com>
2019-08-14 21:21:10 +0800

    feat(preprocessors): complete code for ner preprocessing

--- fancy_nlp/preprocessors/ner_preprocessor.py
+++ fancy_nlp/preprocessors/ner_preprocessor.py
@@ -1,36 +1,68 @@
 # -*- coding: utf-8 -*-
 
+from absl import logging
+import jieba
 from fancy_nlp.preprocessors.preprocessor import Preprocessor
 
 
 class NERPreprocessor(Preprocessor):
     """NER preprocessor.
     """
-    def __init__(self):
-        super(NERPreprocessor, self).__init__()
-        pass
-
-    def load_corpus(self):
-        """Load corpus.
+    def __init__(self, train_data, train_label, min_count=3, start_index=2, use_word=False,
+                 external_word_dict=None, char_embed_type=None, word_embed_type=None,
+                 max_len=None, padding_mode='post',
+                 truncating_mode='post'):
         """
-        pass
 
-    def get_vocabulary(self):
-        """Get vocabulary list with frequency.
+        Args:
+            train_data: a list of tokenized (in char level) sentences
+            train_label: list of list, train_data's labels
+            min_count:
+            start_index:
+            use_word: whether to use word as additional input (here we use char as main input for
+                      Chinese NER)
+            external_word_dict: external word dictionary
+            char_embed_type: str, can be a pre-trained embedding filename or pre-trained embedding
+                             methods (word2vec, glove, fastext)
+            word_embed_type:
+            max_len:
+            padding_mode:
+            truncating_mode:
         """
-        pass
+        self.train_data = train_data
+        self.train_label = train_label
+        self.min_count = min_count
+        self.start_index = start_index
+        self.use_word = use_word
+        self.external_word_dict = external_word_dict
 
-    def get_vocabulary2id(self):
-        """Get a dictionary that key is `token`, and value is `token_id`.
-        """
-        pass
+        super(NERPreprocessor, self).__init__(max_len, padding_mode, truncating_mode)
 
-    def get_id2vocabulary(self):
-        """Get a dictionary that key is `token_id`, and value is `token`.
-        """
-        pass
+        # build character vocabulary
+        self.char_vocab_count, self.char_vocab, self.id2char = self.build_vocab(self.train_data,
+                                                                                self.min_count,
+                                                                                self.start_index)
+        self.char_embeddings = self.build_embedding(char_embed_type, self.char_vocab,
+                                                    self.train_data, pad_idx=0, unk_idx=1)
 
-    def build_id_sequence(self):
-        """Given a list, each item is a token list, return the corresponding id sequence.
+        # build word vocabulary
+        if self.use_word:
+            if self.external_word_dict:
+                for word in self.external_word_dict:
+                    jieba.add_word(word, freq=1000000)
+            untokenized_texts = [''.join(text) for text in self.train_data]
+            word_corpus = self.build_corpus(untokenized_texts, cut_func=lambda x: jieba.lcut(x))
+            self.word_vocab_count, self.word_vocab, self.id2word = self.build_vocab(word_corpus,
+                                                                                    self.min_count,
+                                                                                    self.start_index)
+            self.word_embeddings = self.build_embedding(word_embed_type, self.word_vocab,
+                                                        self.train_data, pad_idx=0, unk_idx=1)
+
+        # build label vocabulary
+        _, self.label_vocab, self.id2label = self.build_label_vocab(self.train_label)
+
+    def build_label_vocab(self, labels):
+        """Build label vocabulary
         """
-        pass
+        return self.build_vocab(labels, min_count=0, start_index=0)
+
--- fancy_nlp/preprocessors/preprocessor.py
+++ fancy_nlp/preprocessors/preprocessor.py
@@ -1,42 +1,94 @@
 # -*- coding: utf-8 -*-
 
+from absl import logging
 from keras.preprocessing.sequence import pad_sequences
+from ..utils import load_pre_trained, train_w2v, train_glove, train_fasttext
 
 
 class Preprocessor(object):
-    """Base preprocessor.
+    """Base Preprocessor.
     """
 
     def __init__(self,
-                 max_len=50,
+                 max_len=None,
+                 padding_mode='post',
                  truncating_mode='post'):
         self.max_len = max_len
+        self.padding_mode = padding_mode
         self.truncating_mode = truncating_mode
 
-    def load_corpus(self):
-        """Load corpus.
+    @staticmethod
+    def build_corpus(untokenized_texts, cut_func):
+        """Build corpus.
+
+        Args:
+            cut_func: function to tokenize texts. For example, lambda x: list(x) can be used for
+                      tokenize texts in char level, while lambda x: jieba.lcut(x) can be used for
+                      tokenize Chinese texts in word level.
+
+        Returns: list of tokenized texts, like ``[['我', '是', '中', '国', '人']]``
+
         """
+        corpus = []
+        for text in untokenized_texts:
+            corpus.append(cut_func(text))
+        return corpus
+
+    @staticmethod
+    def build_vocab(corpus, min_count=3, start_index=2):
+        """Build vocabulary using corpus.
+
+        Args:
+            corpus: list of tokenized texts, like ``[['我', '是', '中', '国', '人']]``
+            min_count: token whose frequency is less than min_count will be ingnored
+            start_index: token's starting index, we usually set it to be 2, which means we preserve
+                         the first 2 indices: 0 for padding token, 1 for "unk" token
+
+        Returns: tuple(dict, dict, dict):
+                 1. token_count: a mapping of tokens to frequencies
+                 2. token_vocab: a mapping of tokens to indices
+                 3. id2token: a mapping of indices to tokens
+
+        """
+        token_count = {}
+        for tokenized_text in corpus:
+            for token in tokenized_text:
+                token_count[token] = token_count.get(token, 0) + 1
+
+        token_count = {token: count for token, count in token_count.items()
+                       if count >= min_count}
+        id2token = {idx + start_index: token for idx, token in enumerate(token_count)}
+        token_vocab = {token: idx for idx, token in id2token}
+
+        logging.info('Build vocabulary finished, vocabulary size: {}'.format(len(token_vocab)))
+        return token_count, token_vocab, id2token
+
+    def build_label_vocab(self, labels):
         raise NotImplementedError
 
-    def get_vocabulary(self):
-        """Get vocabulary list with frequency.
+    @staticmethod
+    def build_embedding(self, embed_type, vocab, corpus=None, pad_idx=0, unk_idx=1):
+        """preprae embeddingd for the words in vocab
         """
-        raise NotImplementedError
+        if embed_type is None:
+            return None
+        if embed_type == 'word2vec':
+            return train_w2v(corpus, vocab, pad_idx, unk_idx)
+        elif embed_type == 'fasttext':
+            return train_fasttext(corpus, vocab, pad_idx, unk_idx)
+        elif embed_type == 'glove':
+            return train_glove(corpus, vocab, pad_idx, unk_idx)
+        else:
+            try:
+                return load_pre_trained(embed_type, vocab, pad_idx, unk_idx)
+            except FileNotFoundError:
+                raise ValueError('`embed_type` input error: {}'.format(embed_type))
 
-    def get_vocabulary2id(self):
-        """Get a dictionary that key is `token`, and value is `token_id`.
-        """
-        raise NotImplementedError
-
-    def get_id2vocabulary(self):
-        """Get a dictionary that key is `token_id`, and value is `token`.
-        """
-        raise NotImplementedError
-
-    def build_id_sequence(self):
+    @staticmethod
+    def build_id_sequence(tokenized_texts, vocabulary, unk_idx=1):
         """Given a list, each item is a token list, return the corresponding id sequence.
         """
-        raise NotImplementedError
+        return [[vocabulary.get(token, unk_idx) for token in text] for text in tokenized_texts]
 
     def pad_sequence(self, sequence_list):
         """Given a list, each item is a id sequence, return the padded sequence
