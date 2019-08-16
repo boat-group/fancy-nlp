50f75329b9ebcab1b7a46ac35833c0d5c3576293
alex_yang <2041462766@qq.com>
2019-08-17 00:17:56 +0800

    feat(utils): add testing of load pre-trained embedding

--- tests/fancy_nlp/utils/test_embedding.py
+++ tests/fancy_nlp/utils/test_embedding.py
@@ -3,13 +3,14 @@
 import os
 import numpy as np
 from fancy_nlp.utils import load_ner_data_and_labels
-from fancy_nlp.utils import train_w2v, train_fasttext
+from fancy_nlp.utils import train_w2v, train_fasttext, load_pre_trained
 
 
 class TestEmbedding:
     test_file = os.path.join(os.path.dirname(__file__), '../../../data/ner/msra/example.txt')
+    embedding_file = os.path.join(os.path.dirname(__file__),
+                                  '../../../data/embeddings/Tencent_ChineseEmbedding_example.txt')
 
-    @classmethod
     def setup_class(self):
         self.test_corpus, _ = load_ner_data_and_labels(self.test_file)
         self.test_vocab = dict((token, i+2) for i, token in enumerate(set(self.test_corpus[0])))
@@ -23,4 +24,10 @@ class TestEmbedding:
         emb = train_fasttext(self.test_corpus, self.test_vocab, embedding_dim=10)
         assert emb.shape[0] == len(self.test_vocab) + 2 and emb.shape[1] == 10
         assert not np.any(emb[0])
+
+    def test_load_pre_trained(self):
+        emb = load_pre_trained(self.embedding_file, self.test_vocab)
+        assert emb.shape[0] == len(self.test_vocab) + 2 and emb.shape[1] == 200
+        assert not np.any(emb[0])
+
 
