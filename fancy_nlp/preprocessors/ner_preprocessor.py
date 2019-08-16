9bd46b003eb6b9a5a6571e1b15caa83b38ab3e41
alex_yang <2041462766@qq.com>
2019-08-17 00:07:23 +0800

    feat(ner_preprocessor): add `prepare_input` and `label_decode` method

--- fancy_nlp/preprocessors/ner_preprocessor.py
+++ fancy_nlp/preprocessors/ner_preprocessor.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from absl import logging
+import numpy as np
 import jieba
+from keras.utils.np_utils import to_categorical
 from fancy_nlp.preprocessors.preprocessor import Preprocessor
+from fancy_nlp.utils.other import pad_sequences_2d
 
 
 class NERPreprocessor(Preprocessor):
@@ -15,17 +17,17 @@ class NERPreprocessor(Preprocessor):
         """
 
         Args:
-            train_data: a list of tokenized (in char level) sentences
+            train_data: a list of tokenized (in char level) texts
             train_label: list of list, train_data's labels
-            min_count:
-            start_index:
+            min_count: int, token whose frequency is lower than min_count will be ignored
+            start_index: the starting index of tokens
             use_word: whether to use word as additional input (here we use char as main input for
                       Chinese NER)
             external_word_dict: external word dictionary
             char_embed_type: str, can be a pre-trained embedding filename or pre-trained embedding
                              methods (word2vec, glove, fastext)
-            word_embed_type:
-            max_len:
+            word_embed_type: same as char_embed_type
+            max_len: int, max sequence len
             padding_mode:
             truncating_mode:
         """
@@ -60,9 +62,78 @@ class NERPreprocessor(Preprocessor):
 
         # build label vocabulary
         _, self.label_vocab, self.id2label = self.build_label_vocab(self.train_label)
+        self.num_class = len(self.label_vocab)
 
     def build_label_vocab(self, labels):
         """Build label vocabulary
+
+        Args:
+            labels: list of list of str, the label strings
         """
         return self.build_vocab(labels, min_count=0, start_index=0)
+
+    def prepare_input(self, data, labels=None):
+        """Prepare input (features and labels) for NER model.
+        Here we not only use character embeddings as main input, but also support word embeddings
+        and other hand-crafted features embeddings as additional input as well.
+
+        Args:
+            data: list of tokenized texts (, like ``[['我', '是', '中', '国', '人']]``
+            labels: list of list of str, the corresponding label strings
+
+        Returns:
+            features: id matrix
+            y: label id matrix (only if label is not None)
+
+        """
+        batch_char_ids, batch_word_ids, batch_label_ids = [], [], []
+        for i, char_text in enumerate(data):
+            char_ids = self.build_id_sequence(char_text, self.char_vocab)
+            batch_char_ids.append(char_ids)
+
+            if self.use_word:
+                word_text = jieba.lcut(''.join(char_text))
+                word_ids = self.get_word_ids(word_text)
+                batch_word_ids.append(word_ids)
+
+            if labels is not None:
+                label_ids = self.build_id_sequence(labels[i], self.label_vocab, unk_idx=-1)
+                label_ids = to_categorical(label_ids, self.num_class).astype(int)
+                batch_label_ids.append(label_ids)
+
+        features = [self.pad_sequence(batch_char_ids)]
+        if self.use_word:
+            features.append(self.pad_sequence(batch_word_ids))
+        if len(features) == 1:
+            features = features[0]
+        if not batch_label_ids:
+            return features, None
+        else:
+            y = pad_sequences_2d(batch_label_ids, max_len_1=self.max_len, max_len_2=self.num_class,
+                                 padding=self.padding_mode, truncating=self.truncating_mode)
+            return features, y
+
+    def get_word_ids(self, word_cut, unk_idx=1):
+        """Given a word-level tokenized text, return the corresponding word ids in char-level
+           sequence. We add the same word id to each character in the word.
+
+        Args:
+            word_cut: list of str, like ['我', '是'. '中国人']
+            unk_idx: the index of words that do not appear in vocabulary, we usually set it to 1
+
+        Returns: list of int, id sequence
+
+        """
+        word_ids = []
+        for word in word_cut:
+            for _ in word:
+                word_ids.append(self.word_vocab.get(word, unk_idx))
+        return word_ids
+
+    def label_decode(self, pred_probs, lengths=None):
+        pred_ids = np.argmax(pred_probs, axis=-1)
+        pred_labels = [[self.id2label[label_id] for label_id in ids] for ids in pred_ids]
+        if lengths is not None:
+            pred_labels = [labels[:length] for labels, length in zip(pred_labels, lengths)]
+        return pred_labels
 
