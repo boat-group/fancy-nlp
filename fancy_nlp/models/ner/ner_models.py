d4a8379de974b885679ab7d1d8bdafe5a63a87b0
alex_yang <2041462766@qq.com>
2019-08-21 21:14:25 +0800

    feat(models): add support of bert embedding and bert ner model

--- fancy_nlp/models/ner/ner_models.py
+++ fancy_nlp/models/ner/ner_models.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import os
-
+import tensorflow as tf
 from keras.layers import *
 from keras_contrib.layers import CRF
 from keras_contrib.losses import crf_loss
 from keras_contrib.metrics import crf_accuracy
 from keras.models import Model
-import tensorflow as tf
 
 from fancy_nlp.models.ner.base_ner_model import BaseNERModel
 
@@ -20,16 +18,22 @@ class BiLSTMNER(BaseNERModel):
     def __init__(self,
                  num_class,
                  checkpoint_dir,
-                 char_embeddings,
-                 char_vocab_size,
-                 char_embed_dim,
+                 use_char=True,
+                 char_embeddings=None,
+                 char_vocab_size=-1,
+                 char_embed_dim=-1,
                  char_embed_trainable=False,
+                 use_bert=False,
+                 bert_config_file=None,
+                 bert_checkpoint_file=None,
+                 bert_trainable=False,
                  use_word=False,
                  word_embeddings=None,
                  word_vocab_size=-1,
                  word_embed_dim=-1,
                  word_embed_trainable=False,
-                 dropout=0.5,
+                 max_len=None,
+                 dropout=0.2,
                  rnn_units=150,
                  fc_dim=100,
                  activation='relu',
@@ -43,11 +47,12 @@ class BiLSTMNER(BaseNERModel):
         self.use_crf = use_crf
         self.optimizer = optimizer
         super(BiLSTMNER, self).__init__(checkpoint_dir, model_name if model_name else 'bilstm_ner',
-                                        char_embeddings, char_vocab_size, char_embed_dim,
-                                        char_embed_trainable, use_word, word_embeddings,
-                                        word_vocab_size, word_embed_dim, word_embed_trainable,
-                                        dropout,
-                                        cutsom_objects={'CRF': CRF} if self.use_crf else None)
+                                        use_char, char_embeddings, char_vocab_size, char_embed_dim,
+                                        char_embed_trainable, use_bert, bert_config_file,
+                                        bert_checkpoint_file, bert_trainable, use_word,
+                                        word_embeddings, word_vocab_size, word_embed_dim,
+                                        word_embed_trainable, max_len, dropout,
+                                        {'CRF': CRF} if use_crf else None)
 
     def build_model_arc(self):
         model_inputs, input_embed = self.build_input()
@@ -79,16 +84,22 @@ class BiGRUNER(BaseNERModel):
     def __init__(self,
                  num_class,
                  checkpoint_dir,
-                 char_embeddings,
-                 char_vocab_size,
-                 char_embed_dim,
+                 use_char=True,
+                 char_embeddings=None,
+                 char_vocab_size=-1,
+                 char_embed_dim=-1,
                  char_embed_trainable=False,
+                 use_bert=False,
+                 bert_config_file=None,
+                 bert_checkpoint_file=None,
+                 bert_trainable=False,
                  use_word=False,
                  word_embeddings=None,
                  word_vocab_size=-1,
                  word_embed_dim=-1,
                  word_embed_trainable=False,
-                 dropout=0.5,
+                 max_len=None,
+                 dropout=0.2,
                  rnn_units=150,
                  fc_dim=100,
                  activation='relu',
@@ -102,10 +113,12 @@ class BiGRUNER(BaseNERModel):
         self.use_crf = use_crf
         self.optimizer = optimizer
         super(BiGRUNER, self).__init__(checkpoint_dir, model_name if model_name else 'bigru_ner',
-                                       char_embeddings, char_vocab_size, char_embed_dim,
-                                       char_embed_trainable, use_word, word_embeddings,
-                                       word_vocab_size, word_embed_dim, word_embed_trainable,
-                                       dropout, {'CRF': CRF} if use_crf else None)
+                                       use_char, char_embeddings, char_vocab_size, char_embed_dim,
+                                       char_embed_trainable, use_bert, bert_config_file,
+                                       bert_checkpoint_file, bert_trainable, use_word,
+                                       word_embeddings, word_vocab_size, word_embed_dim,
+                                       word_embed_trainable, max_len, dropout,
+                                       {'CRF': CRF} if use_crf else None)
 
     def build_model_arc(self):
         model_inputs, input_embed = self.build_input()
@@ -137,16 +150,22 @@ class BiLSTMCNNNER(BaseNERModel):
     def __init__(self,
                  num_class,
                  checkpoint_dir,
-                 char_embeddings,
-                 char_vocab_size,
-                 char_embed_dim,
+                 use_char=True,
+                 char_embeddings=None,
+                 char_vocab_size=-1,
+                 char_embed_dim=-1,
                  char_embed_trainable=False,
+                 use_bert=False,
+                 bert_config_file=None,
+                 bert_checkpoint_file=None,
+                 bert_trainable=False,
                  use_word=False,
                  word_embeddings=None,
                  word_vocab_size=-1,
                  word_embed_dim=-1,
                  word_embed_trainable=False,
-                 dropout=0.5,
+                 max_len=None,
+                 dropout=0.2,
                  rnn_units=150,
                  cnn_filters=300,
                  cnn_kernel_size=3,
@@ -165,10 +184,13 @@ class BiLSTMCNNNER(BaseNERModel):
         self.optimizer = optimizer
         super(BiLSTMCNNNER, self).__init__(checkpoint_dir,
                                            model_name if model_name else 'bilstm_cnn_ner',
-                                           char_embeddings, char_vocab_size, char_embed_dim,
-                                           char_embed_trainable, use_word, word_embeddings,
+                                           use_char, char_embeddings, char_vocab_size,
+                                           char_embed_dim, char_embed_trainable,
+                                           use_bert, bert_config_file, bert_checkpoint_file,
+                                           bert_trainable, use_word, word_embeddings,
                                            word_vocab_size, word_embed_dim, word_embed_trainable,
-                                           dropout, {'CRF': CRF} if use_crf else None)
+                                           max_len, dropout,
+                                           custom_objects={'CRF': CRF} if use_crf else None)
 
     def build_model_arc(self):
         model_inputs, input_embed = self.build_input()
@@ -203,16 +225,22 @@ class BiGRUCNNNER(BaseNERModel):
     def __init__(self,
                  num_class,
                  checkpoint_dir,
-                 char_embeddings,
-                 char_vocab_size,
-                 char_embed_dim,
+                 use_char=True,
+                 char_embeddings=None,
+                 char_vocab_size=-1,
+                 char_embed_dim=-1,
                  char_embed_trainable=False,
+                 use_bert=False,
+                 bert_config_file=None,
+                 bert_checkpoint_file=None,
+                 bert_trainable=False,
                  use_word=False,
                  word_embeddings=None,
                  word_vocab_size=-1,
                  word_embed_dim=-1,
                  word_embed_trainable=False,
-                 dropout=0.5,
+                 max_len=None,
+                 dropout=0.2,
                  rnn_units=150,
                  cnn_filters=300,
                  cnn_kernel_size=3,
@@ -231,10 +259,13 @@ class BiGRUCNNNER(BaseNERModel):
         self.optimizer = optimizer
         super(BiGRUCNNNER, self).__init__(checkpoint_dir,
                                           model_name if model_name else 'bigru_cnn_ner',
-                                          char_embeddings, char_vocab_size, char_embed_dim,
-                                          char_embed_trainable, use_word, word_embeddings,
+                                          use_char, char_embeddings, char_vocab_size,
+                                          char_embed_dim, char_embed_trainable,
+                                          use_bert, bert_config_file, bert_checkpoint_file,
+                                          bert_trainable, use_word, word_embeddings,
                                           word_vocab_size, word_embed_dim, word_embed_trainable,
-                                          dropout, cutsom_objects={'CRF': CRF} if use_crf else None)
+                                          max_len, dropout,
+                                          custom_objects={'CRF': CRF} if use_crf else None)
 
     def build_model_arc(self):
         model_inputs, input_embed = self.build_input()
@@ -246,6 +277,56 @@ class BiGRUCNNNER(BaseNERModel):
         input_encode = Conv1D(filters=self.cnn_filters, kernel_size=self.cnn_kernel_size,
                               padding='same', activation='relu')(input_encode)
         input_encode = TimeDistributed(Dense(self.fc_fim, activation=self.activation))(input_encode)
+
+        if self.use_crf:
+            crf = CRF(units=self.num_class)
+            ner_tag = crf(input_encode)
+            ner_loss = crf_loss
+            ner_metrics = crf_accuracy
+        else:
+            ner_tag = TimeDistributed(Dense(self.num_class, activation='softmax'))(input_encode)
+            ner_loss = 'categorical_crossentropy'
+            ner_metrics = 'accuracy'
+        ner_model = Model(model_inputs if len(model_inputs) > 1 else model_inputs[0], ner_tag)
+        ner_model.compile(optimizer=self.optimizer, loss=ner_loss, metrics=[ner_metrics])
+        return ner_model
+
+
+class BertNER(BaseNERModel):
+    """Bidirectional LSTM model for NER.
+        Support using CUDANNLSTM for acceleration when gpu is available.
+        Support using CRF layer.
+        """
+
+    def __init__(self,
+                 num_class,
+                 checkpoint_dir,
+                 bert_config_file,
+                 bert_checkpoint_file,
+                 bert_trainable,
+                 max_len,
+                 dropout=0.2,
+                 fc_dim=100,
+                 activation='relu',
+                 use_crf=True,
+                 optimizer='adam',
+                 model_name=None):
+        self.num_class = num_class
+        self.fc_fim = fc_dim
+        self.activation = activation
+        self.use_crf = use_crf
+        self.optimizer = optimizer
+        super(BertNER, self).__init__(checkpoint_dir, model_name if model_name else 'bilstm_ner',
+                                      use_char=False, use_bert=True,
+                                      bert_config_file=bert_config_file,
+                                      bert_checkpoint_file=bert_checkpoint_file,
+                                      bert_trainable=bert_trainable, use_word=False,
+                                      max_len=max_len, dropout=dropout,
+                                      custom_objects={'CRF': CRF} if use_crf else None)
+
+    def build_model_arc(self):
+        model_inputs, input_embed = self.build_input()
+        input_encode = TimeDistributed(Dense(self.fc_fim, activation=self.activation))(input_embed)
 
         if self.use_crf:
             crf = CRF(units=self.num_class)
