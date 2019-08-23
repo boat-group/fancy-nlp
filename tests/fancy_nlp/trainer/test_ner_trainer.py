f565caa4d549b9dd4803df1a57710dcb609a3ef7
alex_yang <2041462766@qq.com>
2019-08-23 23:36:08 +0800

    test(tests): adapt to the change of ner trainer

--- tests/fancy_nlp/trainer/test_ner_trainer.py
+++ tests/fancy_nlp/trainer/test_ner_trainer.py
@@ -9,13 +9,24 @@ from fancy_nlp.trainers import NERTrainer
 
 class TestNERTrainer:
     test_file = os.path.join(os.path.dirname(__file__), '../../../data/ner/msra/example.txt')
+    bert_vocab_file = os.path.join(os.path.dirname(__file__),
+                                   '../../../data/embeddings/bert_sample_model/vocab.txt')
+    bert_config_file = os.path.join(os.path.dirname(__file__),
+                                    '../../../data/embeddings/bert_sample_model/bert_config.json')
+    bert_model_file = os.path.join(os.path.dirname(__file__),
+                                   '../../../data/embeddings/bert_sample_model/bert_model.ckpt')
 
     def setup_class(self):
         self.train_data, self.train_labels, self.valid_data, self.valid_labels = \
             load_ner_data_and_labels(self.test_file, split=True)
         self.preprocessor = NERPreprocessor(self.train_data+self.valid_data,
-                                            self.train_labels+self.valid_labels, use_word=True,
-                                            char_embed_type='word2vec', word_embed_type='word2vec')
+                                            self.train_labels+self.valid_labels,
+                                            use_bert=True,
+                                            use_word=True,
+                                            bert_vocab_file=self.bert_vocab_file,
+                                            char_embed_type='word2vec',
+                                            word_embed_type='word2vec',
+                                            max_len=16)
         self.num_class = self.preprocessor.num_class
         self.char_embeddings = self.preprocessor.char_embeddings
         self.char_vocab_size = self.preprocessor.char_vocab_size
@@ -27,18 +38,39 @@ class TestNERTrainer:
         self.checkpoint_dir = os.path.dirname(__file__)
 
         self.ner_model = BiLSTMCNNNER(num_class=self.num_class,
-                                      checkpoint_dir=self.checkpoint_dir,
+                                      use_char=True,
                                       char_embeddings=self.char_embeddings,
                                       char_vocab_size=self.char_vocab_size,
                                       char_embed_dim=self.char_embed_dim,
                                       char_embed_trainable=False,
+                                      use_bert=True,
+                                      bert_config_file=self.bert_config_file,
+                                      bert_checkpoint_file=self.bert_model_file,
                                       use_word=True,
                                       word_embeddings=self.word_embeddings,
                                       word_vocab_size=self.word_vocab_size,
                                       word_embed_dim=self.word_embed_dim,
                                       word_embed_trainable=False,
-                                      use_crf=True)
-        self.ner_model.build_model()
+                                      max_len=self.preprocessor.max_len,
+                                      use_crf=True).build_model()
+
+        self.swa_model = BiLSTMCNNNER(num_class=self.num_class,
+                                      use_char=True,
+                                      char_embeddings=self.char_embeddings,
+                                      char_vocab_size=self.char_vocab_size,
+                                      char_embed_dim=self.char_embed_dim,
+                                      char_embed_trainable=False,
+                                      use_bert=True,
+                                      bert_config_file=self.bert_config_file,
+                                      bert_checkpoint_file=self.bert_model_file,
+                                      use_word=True,
+                                      word_embeddings=self.word_embeddings,
+                                      word_vocab_size=self.word_vocab_size,
+                                      word_embed_dim=self.word_embed_dim,
+                                      word_embed_trainable=False,
+                                      max_len=self.preprocessor.max_len,
+                                      use_crf=True).build_model()
+
         self.ner_trainer = NERTrainer(self.ner_model, self.preprocessor)
 
         self.json_file = os.path.join(self.checkpoint_dir, 'bilstm_cnn_ner.json')
@@ -52,18 +84,22 @@ class TestNERTrainer:
 
     def test_train_no_crf(self):
         ner_model = BiLSTMCNNNER(num_class=self.num_class,
-                                 checkpoint_dir=self.checkpoint_dir,
+                                 use_char=True,
                                  char_embeddings=self.char_embeddings,
                                  char_vocab_size=self.char_vocab_size,
                                  char_embed_dim=self.char_embed_dim,
                                  char_embed_trainable=False,
+                                 use_bert=True,
+                                 bert_config_file=self.bert_config_file,
+                                 bert_checkpoint_file=self.bert_model_file,
                                  use_word=True,
                                  word_embeddings=self.word_embeddings,
                                  word_vocab_size=self.word_vocab_size,
                                  word_embed_dim=self.word_embed_dim,
                                  word_embed_trainable=False,
-                                 use_crf=False)
-        ner_model.build_model()
+                                 max_len=self.preprocessor.max_len,
+                                 use_crf=False).build_model()
+
         ner_trainer = NERTrainer(ner_model, self.preprocessor)
         ner_trainer.train(self.train_data, self.train_labels, self.valid_data, self.valid_labels,
                           batch_size=2, epochs=7)
@@ -72,17 +108,54 @@ class TestNERTrainer:
 
     def test_train_no_word(self):
         preprocessor = NERPreprocessor(self.train_data+self.valid_data,
-                                       self.train_labels+self.valid_labels, use_word=False,
+                                       self.train_labels+self.valid_labels,
+                                       use_bert=True,
+                                       use_word=False,
+                                       bert_vocab_file=self.bert_vocab_file,
+                                       max_len=16,
                                        char_embed_type='word2vec')
-        ner_model = BiLSTMCNNNER(num_class=self.num_class,
-                                 checkpoint_dir=self.checkpoint_dir,
-                                 char_embeddings=self.char_embeddings,
-                                 char_vocab_size=self.char_vocab_size,
-                                 char_embed_dim=self.char_embed_dim,
+        ner_model = BiLSTMCNNNER(num_class=preprocessor.num_class,
+                                 use_char=True,
+                                 char_embeddings=preprocessor.char_embeddings,
+                                 char_vocab_size=preprocessor.char_vocab_size,
+                                 char_embed_dim=preprocessor.char_embed_dim,
                                  char_embed_trainable=False,
+                                 use_bert=True,
+                                 bert_config_file=preprocessor.bert_config_file,
+                                 bert_checkpoint_file=preprocessor.bert_model_file,
                                  use_word=False,
-                                 use_crf=True)
-        ner_model.build_model()
+                                 word_embeddings=preprocessor.word_embeddings,
+                                 word_vocab_size=preprocessor.word_vocab_size,
+                                 word_embed_dim=preprocessor.word_embed_dim,
+                                 word_embed_trainable=False,
+                                 max_len=preprocessor.max_len,
+                                 use_crf=True).build_model()
+
+        ner_trainer = NERTrainer(ner_model, preprocessor)
+        ner_trainer.train(self.train_data, self.train_labels, self.valid_data, self.valid_labels,
+                          batch_size=2, epochs=7)
+        assert not os.path.exists(self.json_file)
+        assert not os.path.exists(self.weights_file)
+
+    def test_train_no_bert(self):
+        preprocessor = NERPreprocessor(self.train_data + self.valid_data,
+                                       self.train_labels + self.valid_labels,
+                                       use_word=True,
+                                       char_embed_type='word2vec')
+        ner_model = BiLSTMCNNNER(num_class=preprocessor.num_class,
+                                 use_char=True,
+                                 char_embeddings=preprocessor.char_embeddings,
+                                 char_vocab_size=preprocessor.char_vocab_size,
+                                 char_embed_dim=preprocessor.char_embed_dim,
+                                 char_embed_trainable=False,
+                                 use_word=True,
+                                 word_embeddings=preprocessor.word_embeddings,
+                                 word_vocab_size=preprocessor.word_vocab_size,
+                                 word_embed_dim=preprocessor.word_embed_dim,
+                                 word_embed_trainable=False,
+                                 max_len=preprocessor.max_len,
+                                 use_crf=True).build_model()
+
         ner_trainer = NERTrainer(ner_model, preprocessor)
         ner_trainer.train(self.train_data, self.train_labels, self.valid_data, self.valid_labels,
                           batch_size=2, epochs=7)
@@ -97,15 +170,22 @@ class TestNERTrainer:
     def test_train_callbacks(self):
         self.ner_trainer.train(self.train_data, self.train_labels, self.valid_data,
                                self.valid_labels, batch_size=2, epochs=7,
-                               callback_list=['modelcheckpoint', 'earlystopping'])
+                               callback_list=['modelcheckpoint', 'earlystopping'],
+                               checkpoint_dir=os.path.dirname(__file__),
+                               model_name='bilstm_cnn_ner')
 
         assert not os.path.exists(self.json_file)
         assert os.path.exists(self.weights_file)
         os.remove(self.weights_file)
+        assert not os.path.exists(self.weights_file)
 
     def test_train_swa(self):
         self.ner_trainer.train(self.train_data, self.train_labels, self.valid_data,
-                               self.valid_labels, batch_size=2, epochs=7, callback_list=['swa'])
+                               self.valid_labels, batch_size=2, epochs=7, callback_list=['swa'],
+                               checkpoint_dir=os.path.dirname(__file__),
+                               model_name='bilstm_cnn_ner',
+                               swa_model=self.swa_model,
+                               load_swa_model=True)
 
         assert not os.path.exists(self.json_file)
         assert not os.path.exists(self.weights_file)
@@ -114,13 +194,12 @@ class TestNERTrainer:
         weights_file = os.path.join(self.checkpoint_dir, 'bilstm_cnn_ner_swa.hdf5')
         assert not os.path.exists(json_file)
         assert os.path.exists(weights_file)
-        self.ner_model.load_swa_model()
         os.remove(weights_file)
+        assert not os.path.exists(weights_file)
 
     def test_generator(self):
         self.ner_trainer.train_generator(self.train_data, self.train_labels,
-                                         self.valid_data, self.valid_labels, batch_size=2, epochs=7,
-                                         shuffle=True)
+                                         self.valid_data, self.valid_labels, batch_size=2, epochs=7)
 
         assert not os.path.exists(self.json_file)
         assert not os.path.exists(self.weights_file)
