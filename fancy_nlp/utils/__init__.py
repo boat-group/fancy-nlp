08605b815d287bd13de45f6543ddfa18b6450c77
alex_yang <2041462766@qq.com>
2019-08-23 22:24:08 +0800

    style(utils): import utility function from other.py

--- fancy_nlp/utils/__init__.py
+++ fancy_nlp/utils/__init__.py
@@ -5,3 +5,4 @@ from .data_loader import load_ner_data_and_labels
 from .embedding import load_pre_trained, train_w2v, train_fasttext
 from .data_generator import NERGenerator
 from .save_load_model import load_keras_model, save_keras_model
+from .other import pad_sequences_2d, get_most_len, get_custom_objects
