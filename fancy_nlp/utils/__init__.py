0f2eb95982a99329a5342c2e2a033360027e5645
alex_yang <2041462766@qq.com>
2019-08-29 00:28:00 +0800

    refactor: change method name

--- fancy_nlp/utils/__init__.py
+++ fancy_nlp/utils/__init__.py
@@ -5,4 +5,4 @@ from .data_loader import load_ner_data_and_labels
 from .embedding import load_pre_trained, train_w2v, train_fasttext
 from .data_generator import NERGenerator
 from .save_load_model import load_keras_model, save_keras_model
-from .other import pad_sequences_2d, get_most_len, get_custom_objects
+from .other import pad_sequences_2d, get_len_from_corpus, get_custom_objects, ChineseBertTokenizer
