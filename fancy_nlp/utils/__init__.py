aeb7f1d12db7b5e6cff18a7882231222ed98a8cb
Shijia E <e.shijia@gmail.com>
2019-08-30 13:20:55 +0800

    feat: export TextClassificationGenerator

--- fancy_nlp/utils/__init__.py
+++ fancy_nlp/utils/__init__.py
@@ -3,6 +3,6 @@
 from __future__ import absolute_import
 from .data_loader import load_ner_data_and_labels
 from .embedding import load_pre_trained, train_w2v, train_fasttext
-from .data_generator import NERGenerator
+from .data_generator import NERGenerator, TextClassificationGenerator
 from .save_load_model import load_keras_model, save_keras_model
 from .other import pad_sequences_2d, get_len_from_corpus, get_custom_objects, ChineseBertTokenizer
