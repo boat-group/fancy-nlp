47c748429f3a3cb02fdec6dbcdebd524c23cb871
Shijia E <e.shijia@gmail.com>
2019-08-31 00:17:15 +0800

    feat: add model storage address

--- fancy_nlp/config.py
+++ fancy_nlp/config.py
@@ -5,3 +5,6 @@ import os
 CACHE_DIR = '~/.fancy_cache'
 if not os.path.exists(os.path.expanduser(CACHE_DIR)):
     os.makedirs(os.path.expanduser(CACHE_DIR))
+
+MODEL_STORAGE_PREFIX = \
+    'https://fancy-nlp-1253403094.cos.ap-shanghai.myqcloud.com/pretrained_models/'
