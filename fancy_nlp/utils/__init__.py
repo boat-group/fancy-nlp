f8e5a37c474ed12b3d9948eded072356f7205407
Shijia E <e.shijia@gmail.com>
2019-08-31 00:18:20 +0800

    feat: add text classification data loader

--- fancy_nlp/utils/__init__.py
+++ fancy_nlp/utils/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-from .data_loader import load_ner_data_and_labels
+from .data_loader import load_ner_data_and_labels, load_text_classification_data_and_labels
 from .embedding import load_pre_trained, train_w2v, train_fasttext
 from .data_generator import NERGenerator, TextClassificationGenerator
 from .save_load_model import load_keras_model, save_keras_model
--- fancy_nlp/utils/data_loader.py
+++ fancy_nlp/utils/data_loader.py
@@ -54,3 +54,50 @@ def load_ner_data_and_labels(filename, delimiter='\t', split=False, split_size=0
         return x_train, y_train, x_test, y_test
     else:
         return token_seqs, label_seqs
+
+
+def load_text_classification_data_and_labels(
+        filename, label_index=0, text_index=1, delimiter='\t', split_mode=0, split_size=0.1,
+        use_header=False, seed=42):
+    """Load text classification data and label from a file.
+
+    Args:
+        filename: str, path to ner file
+        delimiter: str, delimiter to split token and label
+        label_index: int, refer to which column store the classification label
+        text_index:int, refer to which column store the text information
+        split_mode: int, if `split_mode` is 1, it will split the dataset into train and valid;
+                         if `split_mode` is 2, it will split the dataset into train, valid and test
+
+        split_size: float, the proportion of test subset, between 0.0 and 1.0
+        use_header: bool, whether to use first line as the header
+        seed: int, random seed
+
+    Returns: If split: tuple(list, list, list, list), train data and labels as well as test data and
+             labels.
+             Otherwise: tuple(list, list), data and labels
+
+    """
+    with codecs.open(filename, 'r', encoding='utf8') as reader:
+        token_seqs, label_seqs = [], []
+        for i, line in enumerate(reader):
+            if use_header and i == 0:
+                continue
+            line_items = line.strip().split(delimiter)
+
+            text, label = line_items[text_index], line_items[label_index]
+            token_seqs.append(list(text))
+            label_seqs.append(label)
+
+    if split_mode == 1:
+        x_train, x_valid, y_train, y_valid = train_test_split(
+            token_seqs, label_seqs, test_size=split_size, stratify=label_seqs, random_state=seed)
+        return x_train, y_train, x_valid, y_valid
+    elif split_mode == 2:
+        x_train, x_holdout, y_train, y_holdout = train_test_split(
+            token_seqs, label_seqs, test_size=split_size, stratify=label_seqs, random_state=seed)
+        x_valid, x_test, y_valid, y_test = train_test_split(
+            x_holdout, y_holdout, test_size=0.5, stratify=y_holdout, random_state=seed)
+        return x_train, y_train, x_valid, y_valid, x_test, y_test
+    else:
+        return token_seqs, label_seqs
