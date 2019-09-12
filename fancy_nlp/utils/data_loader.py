4c3876b6d10aaf2d64bda6da7e2243f8fed516a8
xushiyao <923290870@qq.com>
2019-09-12 22:02:14 +0800

    feat(utils): add sentence pairs matching(spm) data loader

--- fancy_nlp/utils/data_loader.py
+++ fancy_nlp/utils/data_loader.py
@@ -101,3 +101,52 @@ def load_text_classification_data_and_labels(
         return x_train, y_train, x_valid, y_valid, x_test, y_test
     else:
         return token_seqs, label_seqs
+
+
+def load_spm_data_and_labels(filename, delimiter='\t', split_mode=0, split_size=0.1, seed=42):
+    """Load spm data and label from a file.
+
+    The file should follow fixed format:
+    Each line is a pair of sentences and its label separated by tab, like text_a \t text_b \t label.
+
+    Args:
+        filename: str, path to spm file
+        delimiter: str, delimiter to split texts and label
+        split_mode: int, if `split_mode` is 1, it will split the dataset into train and valid;
+                         if `split_mode` is 2, it will split the dataset into train, valid and test
+        split_size: float, the proportion of test subset, between 0.0 and 1.0
+        seed: int, random seed
+
+    Returns: If split: tuple((list_a, list_b), list, (list_a, list_b), list), train data pairs and
+             labels as well as test data pairs and labels.
+             Otherwise: tuple((list_a, list_b), list), data pairs and labels
+
+    """
+    with codecs.open(filename, 'r', encoding='utf8') as reader:
+        text_a, text_b, labels = [], [], []
+        for i, line in enumerate(reader):
+            line = line.rstrip()
+            line_split = line.split(delimiter)
+            if line:
+                if len(line_split) == 3:
+                    a, b, label = line_split
+                    text_a.append(a)
+                    text_b.append(b)
+                    labels.append(label)
+                else:
+                    raise Exception(f'Format Error at line {i}!'
+                                    f'Input file should follow fixed spm format.')
+    if split_mode == 1:
+        x_a_train, x_a_valid, x_b_train, x_b_valid, y_train, y_valid = train_test_split(
+            text_a, text_b, labels, test_size=split_size, stratify=labels, random_state=seed)
+        return (x_a_train, x_b_train), y_train, (x_a_valid, x_b_valid), y_valid
+    elif split_mode == 2:
+        x_a_train, x_a_holdout, x_b_train, x_b_holdout, y_train, y_holdout = train_test_split(
+            text_a, text_b, labels, test_size=split_size, stratify=labels, random_state=seed)
+        x_a_valid, x_a_test, x_b_valid, x_b_test, y_valid, y_test = train_test_split(
+            x_a_holdout, x_b_holdout, y_holdout, test_size=0.5, stratify=y_holdout,
+            random_state=seed)
+        return (x_a_train, x_b_train), y_train, (x_a_valid, x_b_valid), y_valid, \
+               (x_a_test, x_b_test), y_test
+    else:
+        return (text_a, text_b), labels
