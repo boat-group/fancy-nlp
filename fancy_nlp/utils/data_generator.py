6e307a4e555960f18e1a9397ff142591432773a4
xushiyao <923290870@qq.com>
2019-09-12 22:01:50 +0800

    feat(utils): add sentence pairs matching(spm) data generator

--- fancy_nlp/utils/data_generator.py
+++ fancy_nlp/utils/data_generator.py
@@ -80,3 +80,43 @@ class TextClassificationGenerator(Sequence):
             batch_data = [self.data[i] for i in batch_index]
             batch_labels = None
         return self.preprocessor.prepare_input(batch_data, batch_labels)
+
+
+class SPMGenerator(Sequence):
+    """Data Generator for SPM
+    """
+    def __init__(self, preprocessor, data, labels=None, batch_size=32, shuffle=True):
+        """
+        Args:
+            preprocessor: `SPMPreprocessor` instance to help prepare input for spm model
+            data: list of text pairs (, like ``[['我是中国人', ...], ['我爱中国', ...]]``
+            labels: list of str, the corresponding label strings
+            batch_size: how many samples to train on in one iteration
+            shuffle: whether to shuffle data after each epoch of training
+        """
+        self.preprocessor = preprocessor
+        self.data = data
+        self.labels = labels
+        self.data_size = len(self.data[0])
+        self.batch_size = batch_size
+        self.indices = np.arange(self.data_size)
+        self.steps = int(math.ceil(self.data_size / self.batch_size))
+        self.shuffle = shuffle
+
+    def __len__(self):
+        return self.steps
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+
+    def __getitem__(self, index):
+        batch_index = self.indices[index * self.batch_size: (index + 1) * self.batch_size]
+        if self.labels is not None:
+            batch_data_a, batch_data_b, batch_labels = \
+                zip(*[(self.data[0][i], self.data[1][i], self.labels[i]) for i in batch_index])
+        else:
+            batch_data_a, batch_data_b = zip(*[(self.data[0][i], self.data[1][i])
+                                               for i in batch_index])
+            batch_labels = None
+        return self.preprocessor.prepare_input((batch_data_a, batch_data_b), batch_labels)
