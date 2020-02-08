a0fea9aec3b452ecaea69cc8c2d370628ca6fb46
alex_yang <2041462766@qq.com>
2020-02-08 13:08:33 +0800

    feat(models): change keras to tf.keras

--- fancy_nlp/models/spm/spm_models.py
+++ fancy_nlp/models/spm/spm_models.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-from keras.layers import *
-from keras.models import Model
-from keras.optimizers import Adam
 
 from fancy_nlp.models.spm.base_spm_model import BaseSPMModel
 from fancy_nlp.layers.matching import *
@@ -28,7 +25,6 @@ class SiameseCNN(BaseSPMModel):
                  bert_config_file=None,
                  bert_checkpoint_file=None,
                  bert_trainable=False,
-                 bert_output_layer_num=1,
                  max_len=None,
                  max_word_len=None,
                  dropout=0.2,
@@ -49,8 +45,7 @@ class SiameseCNN(BaseSPMModel):
                                          char_embeddings, char_vocab_size, char_embed_dim,
                                          char_embed_trainable, use_bert, bert_config_file,
                                          bert_checkpoint_file, bert_trainable,
-                                         bert_output_layer_num, False, max_len,
-                                         max_word_len, char_dim, dropout)
+                                         False, max_len, max_word_len, char_dim, dropout)
 
     def build_model(self):
         model_inputs, input_embed_a, input_embed_b = self.build_input()
@@ -58,28 +53,30 @@ class SiameseCNN(BaseSPMModel):
         a_conv_layers = []
         b_conv_layers = []
         for filter_length in self.kernel_size:
-            conv_layer = Conv1D(filters=self.filters, kernel_size=filter_length, padding='valid',
-                                activation='relu', strides=1)
+            conv_layer = tf.keras.layers.Conv1D(filters=self.filters,
+                                                kernel_size=filter_length,
+                                                padding='valid',
+                                                activation='relu',
+                                                strides=1)
             a_conv = conv_layer(input_embed_a)
             b_conv = conv_layer(input_embed_b)
-            a_conv_layers.append(GlobalMaxPooling1D()(a_conv))
-            b_conv_layers.append(GlobalMaxPooling1D()(b_conv))
-        a_conv = concatenate(inputs=a_conv_layers)
-        b_conv = concatenate(inputs=b_conv_layers)
-        sent_rep = concatenate([a_conv, b_conv])
-        sent_rep = Dense(self.fc_dim, activation=self.activation)(sent_rep)
-        matching_score = Dense(self.num_class, activation='softmax')(sent_rep)
+            a_conv_layers.append(tf.keras.layers.GlobalMaxPooling1D()(a_conv))
+            b_conv_layers.append(tf.keras.layers.GlobalMaxPooling1D()(b_conv))
+        a_conv = tf.keras.layers.concatenate(inputs=a_conv_layers)
+        b_conv = tf.keras.layers.concatenate(inputs=b_conv_layers)
+        sent_rep = tf.keras.layers.concatenate([a_conv, b_conv])
+        sent_rep = tf.keras.layers.Dense(self.fc_dim, activation=self.activation)(sent_rep)
+        matching_score = tf.keras.layers.Dense(self.num_class, activation='softmax')(sent_rep)
 
         spm_loss = 'categorical_crossentropy'
         spm_metrics = 'accuracy'
-        spm_model = Model(model_inputs, matching_score)
+        spm_model = tf.keras.models.Model(model_inputs, matching_score)
         spm_model.compile(optimizer=self.optimizer, loss=spm_loss, metrics=[spm_metrics])
         return spm_model
 
 
 class SiameseBiLSTM(BaseSPMModel):
     """Siamese Bidirectional LSTM model for SPM.
-    Support using CUDANNLSTM for acceleration when gpu is available.
     """
     def __init__(self,
                  num_class,
@@ -97,7 +94,6 @@ class SiameseBiLSTM(BaseSPMModel):
                  bert_config_file=None,
                  bert_checkpoint_file=None,
                  bert_trainable=False,
-                 bert_output_layer_num=1,
                  max_len=None,
                  max_word_len=None,
                  dropout=0.2,
@@ -116,33 +112,28 @@ class SiameseBiLSTM(BaseSPMModel):
                                             char_embeddings, char_vocab_size, char_embed_dim,
                                             char_embed_trainable, use_bert, bert_config_file,
                                             bert_checkpoint_file, bert_trainable,
-                                            bert_output_layer_num, False, max_len,
-                                            max_word_len, char_dim, dropout)
+                                            False, max_len, max_word_len, char_dim, dropout)
 
     def build_model(self):
         model_inputs, input_embed_a, input_embed_b = self.build_input()
 
-        if tf.test.is_gpu_available(cuda_only=True):
-            bilstm_layer = Bidirectional(CuDNNLSTM(self.rnn_units))
-        else:
-            bilstm_layer = Bidirectional(LSTM(self.rnn_units))
+        bilstm_layer = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.rnn_units))
         a_lstm = bilstm_layer(input_embed_a)
         b_lstm = bilstm_layer(input_embed_b)
 
-        sent_rep = concatenate([a_lstm, b_lstm])
-        sent_rep = Dense(self.fc_dim, activation=self.activation)(sent_rep)
-        matching_score = Dense(self.num_class, activation='softmax')(sent_rep)
+        sent_rep = tf.keras.layers.concatenate([a_lstm, b_lstm])
+        sent_rep = tf.keras.layers.Dense(self.fc_dim, activation=self.activation)(sent_rep)
+        matching_score = tf.keras.layers.Dense(self.num_class, activation='softmax')(sent_rep)
 
         spm_loss = 'categorical_crossentropy'
         spm_metrics = 'accuracy'
-        spm_model = Model(model_inputs, matching_score)
+        spm_model = tf.keras.models.Model(model_inputs, matching_score)
         spm_model.compile(optimizer=self.optimizer, loss=spm_loss, metrics=[spm_metrics])
         return spm_model
 
 
 class SiameseBiGRU(BaseSPMModel):
     """Siamese Bidirectional GRU model for SPM.
-    Support using CUDANNGRU for acceleration when gpu is available.
     """
 
     def __init__(self,
@@ -180,26 +171,22 @@ class SiameseBiGRU(BaseSPMModel):
                                            char_embeddings, char_vocab_size, char_embed_dim,
                                            char_embed_trainable, use_bert, bert_config_file,
                                            bert_checkpoint_file, bert_trainable,
-                                           bert_output_layer_num, False, max_len,
-                                           max_word_len, char_dim, dropout)
+                                           False, max_len, max_word_len, char_dim, dropout)
 
     def build_model(self):
         model_inputs, input_embed_a, input_embed_b = self.build_input()
 
-        if tf.test.is_gpu_available(cuda_only=True):
-            bigru_layer = Bidirectional(CuDNNGRU(self.rnn_units))
-        else:
-            bigru_layer = Bidirectional(GRU(self.rnn_units))
+        bigru_layer = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(self.rnn_units))
         a_gru = bigru_layer(input_embed_a)
         b_gru = bigru_layer(input_embed_b)
 
-        sent_rep = concatenate([a_gru, b_gru])
-        sent_rep = Dense(self.fc_dim, activation=self.activation)(sent_rep)
-        matching_score = Dense(self.num_class, activation='softmax')(sent_rep)
+        sent_rep = tf.keras.layers.concatenate([a_gru, b_gru])
+        sent_rep = tf.keras.layers.Dense(self.fc_dim, activation=self.activation)(sent_rep)
+        matching_score = tf.keras.layers.Dense(self.num_class, activation='softmax')(sent_rep)
 
         spm_loss = 'categorical_crossentropy'
         spm_metrics = 'accuracy'
-        spm_model = Model(model_inputs, matching_score)
+        spm_model = tf.keras.models.Model(model_inputs, matching_score)
         spm_model.compile(optimizer=self.optimizer, loss=spm_loss, metrics=[spm_metrics])
         return spm_model
 
@@ -243,63 +230,60 @@ class ESIM(BaseSPMModel):
                                    char_embeddings, char_vocab_size,  char_embed_dim,
                                    char_embed_trainable, use_bert, bert_config_file,
                                    bert_checkpoint_file, bert_trainable,
-                                   bert_output_layer_num, False, max_len,
-                                   max_word_len, char_dim, dropout)
+                                   False, max_len, max_word_len, char_dim, dropout)
 
     def build_model(self):
         model_inputs, input_embed_a, input_embed_b = self.build_input()
 
-        if tf.test.is_gpu_available(cuda_only=True):
-            bilstm_layer = Bidirectional(CuDNNLSTM(self.rnn_units, return_sequences=True))
-        else:
-            bilstm_layer = Bidirectional(LSTM(self.rnn_units, return_sequences=True))
+        bilstm_layer = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(self.rnn_units, return_sequences=True))
         a_lstm = bilstm_layer(input_embed_a)
         b_lstm = bilstm_layer(input_embed_b)
 
         # interaction attention
-        attention = Dot(axes=-1)([a_lstm, b_lstm])
+        attention = tf.keras.layers.Dot(axes=-1)([a_lstm, b_lstm])
 
-        wb = Lambda(lambda x: K.softmax(x, axis=1), output_shape=lambda x: x)(attention)
-        wa = Permute((2, 1))(Lambda(lambda x: K.softmax(x, axis=2), output_shape=lambda x: x)(attention))
-        a_ = Dot(axes=1)([wa, b_lstm])
-        b_ = Dot(axes=1)([wb, a_lstm])
+        wb = tf.keras.layers.Lambda(lambda x: K.softmax(x, axis=1),
+                                    output_shape=lambda x: x)(attention)
+        wa = tf.keras.layers.Lambda(lambda x: K.softmax(x, axis=2),
+                                    output_shape=lambda x: x)(attention)
+        wa = tf.keras.layers.Permute((2, 1))(wa)
+        a_ = tf.keras.layers.Dot(axes=1)([wa, b_lstm])
+        b_ = tf.keras.layers.Dot(axes=1)([wb, a_lstm])
 
-        substract_a = Subtract()([a_lstm, a_])
-        multiply_a = Multiply()([a_lstm, a_])
-        substract_b = Subtract()([b_lstm, b_])
-        multiply_b = Multiply()([b_lstm, b_])
+        substract_a = tf.keras.layers.Subtract()([a_lstm, a_])
+        multiply_a = tf.keras.layers.Multiply()([a_lstm, a_])
+        substract_b = tf.keras.layers.Subtract()([b_lstm, b_])
+        multiply_b = tf.keras.layers.Multiply()([b_lstm, b_])
 
-        m_a = concatenate([a_lstm, a_, substract_a, multiply_a], axis=-1)
-        m_b = concatenate([b_lstm, b_, substract_b, multiply_b], axis=-1)
+        m_a = tf.keras.layers.concatenate([a_lstm, a_, substract_a, multiply_a], axis=-1)
+        m_b = tf.keras.layers.concatenate([b_lstm, b_, substract_b, multiply_b], axis=-1)
 
-        if tf.test.is_gpu_available(cuda_only=True):
-            compose = Bidirectional(CuDNNLSTM(self.rnn_units, return_sequences=True))
-        else:
-            compose = Bidirectional(LSTM(self.rnn_units, return_sequences=True))
+        compose = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(self.rnn_units, return_sequences=True))
         v_a = compose(m_a)
         v_b = compose(m_b)
 
-        a_maxpool = GlobalMaxPool1D()(v_a)
-        b_maxpool = GlobalMaxPool1D()(v_b)
-        a_avgpool = GlobalAvgPool1D()(v_a)
-        b_avgpool = GlobalAvgPool1D()(v_b)
-        a_pool = concatenate([a_avgpool, a_maxpool], axis=-1)
-        b_pool = concatenate([b_avgpool, b_maxpool], axis=-1)
+        a_maxpool = tf.keras.layers.GlobalMaxPool1D()(v_a)
+        b_maxpool = tf.keras.layers.GlobalMaxPool1D()(v_b)
+        a_avgpool = tf.keras.layers.GlobalAvgPool1D()(v_a)
+        b_avgpool = tf.keras.layers.GlobalAvgPool1D()(v_b)
+        a_pool = tf.keras.layers.concatenate([a_avgpool, a_maxpool], axis=-1)
+        b_pool = tf.keras.layers.concatenate([b_avgpool, b_maxpool], axis=-1)
 
-        sent_rep = concatenate([a_pool, b_pool])
-        sent_rep = Dense(self.fc_dim, activation=self.activation)(sent_rep)
-        matching_score = Dense(self.num_class, activation='softmax')(sent_rep)
+        sent_rep = tf.keras.layers.concatenate([a_pool, b_pool])
+        sent_rep = tf.keras.layers.Dense(self.fc_dim, activation=self.activation)(sent_rep)
+        matching_score = tf.keras.layers.Dense(self.num_class, activation='softmax')(sent_rep)
 
         spm_loss = 'categorical_crossentropy'
         spm_metrics = 'accuracy'
-        spm_model = Model(model_inputs, matching_score)
+        spm_model = tf.keras.models.Model(model_inputs, matching_score)
         spm_model.compile(optimizer=self.optimizer, loss=spm_loss, metrics=[spm_metrics])
         return spm_model
 
 
 class BiMPM(BaseSPMModel):
     """Bilateral Multi-perspective Matching (BiMPM) model for SPM.
-    Support using CUDANNLSTM for acceleration when gpu is available.
     """
 
     def __init__(self,
@@ -337,42 +321,40 @@ class BiMPM(BaseSPMModel):
                                     char_embeddings, char_vocab_size, char_embed_dim,
                                     char_embed_trainable, use_bert, bert_config_file,
                                     bert_checkpoint_file, bert_trainable,
-                                    bert_output_layer_num, False, max_len, max_word_len,
-                                    char_dim, dropout)
+                                    False, max_len, max_word_len, char_dim, dropout)
 
     def build_model(self):
         model_inputs, input_embed_a, input_embed_b = self.build_input()
 
-        if tf.test.is_gpu_available(cuda_only=True):
-            bilstm_layer = Bidirectional(CuDNNLSTM(self.rnn_units, return_sequences=True))
-        else:
-            bilstm_layer = Bidirectional(LSTM(self.rnn_units, return_sequences=True))
+        bilstm_layer = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(self.rnn_units, return_sequences=True))
         a_lstm = bilstm_layer(input_embed_a)
         b_lstm = bilstm_layer(input_embed_b)
 
         matching1 = FullMatching()
-        a_matching1 = matching1([a_lstm, GlobalAvgPool1D()(b_lstm)])
-        b_matching1 = matching1([b_lstm, GlobalAvgPool1D()(a_lstm)])
+        a_matching1 = matching1([a_lstm, tf.keras.layers.GlobalAvgPool1D()(b_lstm)])
+        b_matching1 = matching1([b_lstm, tf.keras.layers.GlobalAvgPool1D()(a_lstm)])
 
         matching2_forward = MaxPoolingMatching()
         a_matching2 = matching2_forward([a_lstm, b_lstm])
         b_matching2 = matching2_forward([b_lstm, a_lstm])
 
-        cos = Dot(axes=-1, normalize=True)([a_lstm, b_lstm])
-        wb = Lambda(lambda x: K.softmax(x, axis=1), output_shape=lambda x: x)(cos)
-        wa = Permute((2, 1))(Lambda(lambda x: K.softmax(x, axis=2), output_shape=lambda x: x)(cos))
+        cos = tf.keras.layers.Dot(axes=-1, normalize=True)([a_lstm, b_lstm])
+        wb = tf.keras.layers.Lambda(lambda x: K.softmax(x, axis=1), output_shape=lambda x: x)(cos)
+        wa = tf.keras.layers.Lambda(lambda x: K.softmax(x, axis=2), output_shape=lambda x: x)(cos)
+        wa = tf.keras.layers.Permute((2, 1))(wa)
 
-        a_ = Dot(axes=1)([wa, b_lstm])
-        b_ = Dot(axes=1)([wb, a_lstm])
+        a_ = tf.keras.layers.Dot(axes=1)([wa, b_lstm])
+        b_ = tf.keras.layers.Dot(axes=1)([wb, a_lstm])
 
         matching3 = AttentiveMatching(perspective_num=5)
         a_matching3 = matching3([a_lstm, a_])
         b_matching3 = matching3([b_lstm, b_])
 
-        a_max = Lambda(
+        a_max = tf.keras.layers.Lambda(
             lambda x: K.batch_dot(K.one_hot(K.argmax(x[0], axis=1), K.int_shape(x[1])[-2]), x[1]),
             output_shape=lambda x: x[1])([wb, a_lstm])
-        b_max = Lambda(
+        b_max = tf.keras.layers.Lambda(
             lambda x: K.batch_dot(K.one_hot(K.argmax(x[0], axis=1), K.int_shape(x[1])[-2]), x[1]),
             output_shape=lambda x: x[1])([wa, b_lstm])
 
@@ -380,23 +362,22 @@ class BiMPM(BaseSPMModel):
         a_matching4 = matching4([a_lstm, b_max])
         b_matching4 = matching4([b_lstm, a_max])
 
-        a_matching = concatenate([a_matching1, a_matching2, a_matching3, a_matching4])
-        b_matching = concatenate([b_matching1, b_matching2, b_matching3, b_matching4])
+        a_matching = tf.keras.layers.concatenate([a_matching1, a_matching2,
+                                                  a_matching3, a_matching4])
+        b_matching = tf.keras.layers.concatenate([b_matching1, b_matching2,
+                                                  b_matching3, b_matching4])
 
-        if tf.test.is_gpu_available(cuda_only=True):
-            aggregation_layer = Bidirectional(CuDNNLSTM(self.rnn_units))
-        else:
-            aggregation_layer = Bidirectional(LSTM(self.rnn_units))
+        aggregation_layer = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.rnn_units))
         a_bimpm = aggregation_layer(a_matching)
         b_bimpm = aggregation_layer(b_matching)
 
-        sent_rep = concatenate([a_bimpm, b_bimpm])
-        sent_rep = Dense(self.fc_dim, activation=self.activation)(sent_rep)
-        matching_score = Dense(self.num_class, activation='softmax')(sent_rep)
+        sent_rep = tf.keras.layers.concatenate([a_bimpm, b_bimpm])
+        sent_rep = tf.keras.layers.Dense(self.fc_dim, activation=self.activation)(sent_rep)
+        matching_score = tf.keras.layers.Dense(self.num_class, activation='softmax')(sent_rep)
 
         spm_loss = 'categorical_crossentropy'
         spm_metrics = 'accuracy'
-        spm_model = Model(model_inputs, matching_score)
+        spm_model = tf.keras.models.Model(model_inputs, matching_score)
         spm_model.compile(optimizer=self.optimizer, loss=spm_loss, metrics=[spm_metrics])
         return spm_model
 
@@ -416,7 +397,7 @@ class BertSPM(BaseSPMModel):
                  dropout=0.2,
                  fc_dim=100,
                  activation='tanh',
-                 optimizer=Adam(lr=1e-5),  # use a small learning rate for bert
+                 optimizer=tf.keras.optimizers.Adam(lr=1e-5),  # use a small learning rate for bert
                  bert_output_layer_num=1,
                  **kwargs):
         self.num_class = num_class
@@ -427,19 +408,18 @@ class BertSPM(BaseSPMModel):
                                       bert_config_file=bert_config_file,
                                       bert_checkpoint_file=bert_checkpoint_file,
                                       bert_trainable=bert_trainable,
-                                      bert_output_layer_num=bert_output_layer_num,
                                       use_bert_model=True,
                                       max_len=max_len, dropout=dropout)
 
     def build_model(self):
         model_inputs, input_embed = self.build_input()
 
-        sent_rep = Lambda(lambda x: x[:, 0])(input_embed)
-        sent_rep = Dense(self.fc_dim, activation=self.activation)(sent_rep)
-        matching_score = Dense(self.num_class, activation='softmax')(sent_rep)
+        sent_rep = tf.keras.layers.Lambda(lambda x: x[:, 0])(input_embed)
+        sent_rep = tf.keras.layers.Dense(self.fc_dim, activation=self.activation)(sent_rep)
+        matching_score = tf.keras.layers.Dense(self.num_class, activation='softmax')(sent_rep)
 
         spm_loss = 'categorical_crossentropy'
         spm_metrics = 'accuracy'
-        spm_model = Model(model_inputs, matching_score)
+        spm_model = tf.keras.models.Model(model_inputs, matching_score)
         spm_model.compile(optimizer=self.optimizer, loss=spm_loss, metrics=[spm_metrics])
         return spm_model
